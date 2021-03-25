ff5ecfeebcea3d34f9e2a1488418e652af0b6989
GiuliaRadu <radugiulia17@gmail.com>
2021-03-25 11:40:04 +0200

    Revert "added an error"
    This reverts commit 8a2c59d7b1b674b8a9ab534eaeeab7210fdfeaa0.

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace RazorMvc
 
             app.UseRouting();
 
-            app.UseAuthorization()
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
