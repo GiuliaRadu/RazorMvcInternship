b944133f2068cc415963e869ffac8d36b7bf267f
GiuliaRadu <radugiulia17@gmail.com>
2021-03-25 12:51:23 +0200

    Added HomeController, InternshipClass

---
+++ Models/InternshipClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorMvc.Models
+{
+    public class InternshipClass
+    {
+        private List<string> _members;
+
+        public InternshipClass()
+        {
+            _members = new List<string>
+            {
+                "Collegue1",
+                "Collegue2",
+                "Collegue3",
+            };
+        }
+
+        public IList<string> Members
+        {
+            get { return _members; }
+        }
+    }
+}
