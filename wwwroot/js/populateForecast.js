e97cfdde0eca0c696ce49b26f22f6b3dfcf4d4e5
GiuliaRadu <radugiulia17@gmail.com>
2021-03-29 13:03:16 +0300

    Implement weather forecast on FE

---
+++ wwwroot/js/populateForecast.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: ' /WeatherForecast',
+        success: function (data) {
+            let tomorrow = data[0];
+            let date = new Date(tomorrow.date);
+            let tomorrowDate = date.toDateString();
+            
+           
+            $('#date').text(tomorrowDate);
+            $('#temperature').text(tomorrow.temperatureC, 'C');
+            $('#summary').text(tomorrow.summary);
+        },
+        error: funtion(data) {
+        alert(`failed to load data`);
+        }
+    });
+
+
+function formatDate(jsonDate) {
+    function join(t, a, s) {
+        function format(m) {
+            let f = new Intl.DateTimeFormat('en', m);
+            return f.format(t);
+        }
+        return a.map(format).join(s);
+    }
+
+    let date = new Date(jsonDate);
+    let a = [{ day: 'numeric' }, { month: 'short' }, { year: 'numeric' }];
+    let s = join(date, a, '-');
+    return s;
+}
+
+
+
+
+
+
+
+
