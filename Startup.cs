41c7b6d4ff7f64adefcb8f4db68ce69bc5d42221
GiuliaRadu <radugiulia17@gmail.com>
2021-03-29 13:02:10 +0300

    Implemented Swagger

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using RazorMvc.Services;
 
 namespace RazorMvc
@@ -26,6 +27,10 @@ namespace RazorMvc
         {
             services.AddControllersWithViews();
             services.AddSingleton(typeof(InternshipService));
+			services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RazorMvc.webApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +39,8 @@ namespace RazorMvc
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+				app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RazorMvc.webApi v1"));
             }
             else
             {
