ebc698fa5c2b76d8a403f3b7f75840d4fc2e4f22
GiuliaRadu <radugiulia17@gmail.com>
2021-03-30 18:30:12 +0300

    changed type of TemperatureC

--- Models/WeatherForecast.cs
+++ Models/WeatherForecast.cs
@@ -4,9 +4,18 @@ namespace RazorMvc.webApi
 {
     public class WeatherForecast
     {
+
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public double TemperatureC { get; set; }
+
+        public double TemperatureK
+        {
+            get
+            {
+                return TemperatureC + 273.15;
+            }
+        }
 
         public string Summary { get; set; }
     }
