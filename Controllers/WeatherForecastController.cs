4dfb242c95159d41122955c8a5e88a5fc96beff1
GiuliaRadu <radugiulia17@gmail.com>
2021-03-30 18:28:57 +0300

    Fetched data from API and deserialized json

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RazorMvc.Utilities;
+using RestSharp;
 
 namespace RazorMvc.webApi.Controllers
 {
@@ -38,6 +41,38 @@ namespace RazorMvc.webApi.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)],
             })
             .ToArray();
+        }
+
+        public IList<WeatherForecast> FetchWeatherForecasts(double latitude, double longitude, string apiKey)
+        {
+            var endpoint = $"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=hourly,minutely&appid={apiKey}";
+            var client = new RestClient(endpoint);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            return ConvertResponseContentToListOfWeatherForecast(response.Content);
+        }
+
+        private IList<WeatherForecast> ConvertResponseContentToListOfWeatherForecast(string content)
+        {
+            var apiResponse = JsonSerializer.Deserialize<WeatherApiResponse>(content);
+
+            List <WeatherForecast> forecasts = new List<WeatherForecast>();
+
+            for (int i = 0; i < apiResponse.daily.Length; i++)
+            {
+                double tempK = apiResponse.daily[i].temp.day;
+                string summary = apiResponse.daily[i].weather[0].description;
+
+                forecasts.Add(new WeatherForecast
+                {
+                    TemperatureC = tempK - 273.15,
+                    Summary = summary,
+                });
+
+            }
+
+            return forecasts;
         }
     }
 }
