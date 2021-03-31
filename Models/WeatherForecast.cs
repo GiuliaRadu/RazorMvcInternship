fe7ac73353a331f83d3048f81932d496139542a2
GiuliaRadu <radugiulia17@gmail.com>
2021-03-31 13:06:49 +0300

    simplified temf field

--- Models/WeatherForecast.cs
+++ Models/WeatherForecast.cs
@@ -7,13 +7,10 @@ namespace RazorMvc.webApi
 
         public DateTime Date { get; set; }
 
-        public int TemperatureC
-        {
-            get { return (int)(TemperatureK - 273.15); }
-        }
+        public double TemperatureK { get; set; }
+
+        public int TemperatureC => (int)(TemperatureK - 273.15);
 
         public string Summary { get; set; }
-
-        public double TemperatureK { get; set; }
     }
 }
