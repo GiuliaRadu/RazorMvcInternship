8828a5f9bf7fee8c3ef3a2e99ce4f305dcd4df89
GiuliaRadu <radugiulia17@gmail.com>
2021-04-12 14:00:17 +0300

    Added subscribeOnChanges script

---
+++ wwwroot/js/subscribeOnChanges.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/messagehub").build();
+
+connection.on("AddMember", function (name, id) {
+    alert(`User ${name} with Id = ${id}`);
+});
+
+connection.start().then(function () {
+    console.log("Connection established")
+}).catch(function (err) {
+    return console.error(err.toString());
+});
