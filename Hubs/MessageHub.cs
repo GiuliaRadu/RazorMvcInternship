884632d50a5fdb01425e069c12f3f55933c40ac4
GiuliaRadu <radugiulia17@gmail.com>
2021-04-12 13:06:45 +0300

    Removed db context, added messageService, created Message model

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using RazorMvc.Services;
 
 namespace RazorMvc.Hubs
 {
     public class MessageHub : Hub
     {
+        private readonly MessageService messageService;
+
+        public MessageHub(MessageService messageService)
+        {
+            this.messageService = messageService;
+        }
+
         public async Task SendMessage(string user, string message)
         {
+            messageService.AddMessage(user, message);
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -47,7 +47,7 @@ namespace RazorMvc
             });
 
             services.AddSignalR();
-
+            services.AddSingleton<MessageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
