464073c039e884e1dbc948ee5600bed287f283c2
GiuliaRadu <radugiulia17@gmail.com>
2021-04-14 12:29:05 +0300

    Cleaned up code. Made a separate file for editNewComer.js

--- wwwroot/js/addNewComer.js
+++ wwwroot/js/addNewComer.js
@@ -21,52 +21,7 @@ $(document).ready(function () {
     $("#clear").click(function () {
         $("#newcomer").val("");
     })
-    // Bind event to dynamically created element: https://makitweb.com/attach-event-to-dynamically-created-elements-with-jquery
-    $("#list").on("click", ".delete", function () {
-        var targetMemberTag = $(this).closest('li');
-        var index = targetMemberTag.index(targetMemberTag.parent());
-        $.ajax({
-            url: `/Home/RemoveMember/${index}`,
-            type: 'DELETE',
-            success: function () {
-                targetMemberTag.remove();
-            },
-            error: function () {
-                alert(`Failed to delete member with index=${index}`);
-            }
-        })
-    })
-    $("#list").on("click", ".startEdit", function () {
-        var targetMemberTag = $(this).closest('li');
-        var serverIndex = targetMemberTag.attr('member-id');
-        var clientIndex = targetMemberTag.index();
-        var currentName = targetMemberTag.find(".name").text();
-        $('#editClassmate').attr("member-id", serverIndex);
-        $('#editClassmate').attr("memberIndex", clientIndex);
-        $('#classmateName').val(currentName);
-    })
-    $("#editClassmate").on("click", "#submit", function () {
-        var newName = $('#classmateName').val();
-        var id = $('#editClassmate').attr("member-id");
-        var index = $('#editClassmate').attr("memberIndex");
 
-        $.ajax({
-            contentType: 'application/json',
-            data: JSON.stringify({ "Name": `${newName}` }),
-            method: "PUT",
-            url: `api/Internship/${id}`,
-            success: function (response) {
-                $('.name').eq(index).replaceWith(newName);
-            },
-            error: function (data) {
-                alert(`Failed to update`);
-            }
-        });
-    })
-
-    $("#editClassmate").on("click", "#cancel", function () {
-        console.log('cancel changes');
-    })
     function refreshWeatherForecast() {
         $.ajax({
             url: `/WeatherForecast`,
---
+++ wwwroot/js/editNewComer.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+
+    $("#list").on("click", ".startEdit", function () {
+        var targetMemberTag = $(this).closest('li');
+        var serverIndex = targetMemberTag.attr('member-id');
+        var clientIndex = targetMemberTag.index();
+        var currentName = targetMemberTag.find(".name").text();
+        $('#editClassmate').attr("member-id", serverIndex);
+        $('#editClassmate').attr("memberIndex", clientIndex);
+        $('#classmateName').val(currentName);
+    })
+
+    $("#editClassmate").on("click", "#submit", function () {
+        console.log('submit changes to server');
+
+        var newName = $('#classmateName').val();
+        var id = $('#editClassmate').attr("member-id");
+        var index = $('#editClassmate').attr("memberIndex");
+
+        $.ajax({
+            contentType: 'application/json',
+            data: JSON.stringify({ "Name": `${newName}` }),
+            method: "PUT",
+            url: `api/Internship/${id}`,
+            success: function (response) {
+                $('.name').eq(index).replaceWith(newName);
+            },
+            error: function (data) {
+                alert(`Failed to update`);
+            }
+        });
+    })
+
+    $("#editClassmate").on("click", "#cancel", function () {
+        console.log('cancel changes');
+    })
+});
